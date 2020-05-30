6d4aa99f30125d03c0fbc5ebc3559ecbbc9f1762
xman086 <gotohuangzhs@gmail.com>
2020-05-30 08:14:04 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -16,15 +16,15 @@ jsproxy_config({
       label: '演示服务-香港节点',
       lines: {
         // 主机:权重
-        'node-aliyun-hk-0.etherdream.com:8443': 1,
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 1,
+        //'node-aliyun-hk-0.etherdream.com:8443': 1,
+        //'node-aliyun-hk-1.etherdream.com:8443': 1,
+        //'node-aliyun-hk-2.etherdream.com:8443': 1,
       }
     },
     'demo-sg': {
       label: '演示服务-新加坡节点',
       lines: {
-        'node-aliyun-sg.etherdream.com:8443': 1,
+        //'node-aliyun-sg.etherdream.com:8443': 1,
       },
     },
     'mysite': {
